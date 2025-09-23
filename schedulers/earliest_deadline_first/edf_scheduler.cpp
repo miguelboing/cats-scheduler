6d3d84451c6b6df1344d3039630fad0518a9da48
miguelboing <>
2025-09-23 18:09:30 +0100

    schedulers/earliest_deadline_first: Method to sort queue by deadline

--- schedulers/earliest_deadline_first/edf_scheduler.cpp
+++ schedulers/earliest_deadline_first/edf_scheduler.cpp
@@ -1,20 +1,41 @@
+#include <algorithm>
+
 #include "schedulers/earliest_deadline_first/edf_scheduler.hpp"
 
-class EDF_scheduler: public BaseScheduler {
-public:
-	void addTask(std::shared_ptr<Task> task) {
-		task_queue.push_back(task)
-	}
+schedule_result_t EDF_scheduler::schedule_packets(system_model_t system_model)
+{
+    unsigned int time_frame_counter = 0U;
+    std::vector<packet_t> scheduled_packets;
+    schedule_result_t schedule_result;
 
-	std::vector<std::shared_ptr<Task>> ScheduleTasks() {
-		// Add your scheduling logic here
-		// scheduled_tasks.push_back(some_task);
-		return scheduled_tasks;
-	}
+    BaseScheduler::reset_scheduler();
 
-	std::string getName() const {
-		return "EDF"
-	}
-};
+    scheduled_packets = EDF_scheduler::sort_packets_by_deadline();
 
+    /* TODO:Add logic to iterate between each packet, and advance with the time_frame_counter */
+
+    return schedule_result;
+}
+
+std::string EDF_scheduler::get_name() const {
+    return "EDF";
+}
+
+std::vector<packet_t> EDF_scheduler::sort_packets_by_deadline()
+{
+    if (priv_queue_packet->empty()) {
+        return {};
+    }
+
+    /* Create a copy to avoid modifying original */
+    std::vector<packet_t> sorted_packets = *priv_queue_packet;
+
+    /* Sort by deadline (earliest first) */
+    std::sort(sorted_packets.begin(), sorted_packets.end(),
+        [](const packet_t& a, const packet_t& b) {
+            return a.deadline < b.deadline;
+        });
+
+        return sorted_packets;
+}
 
