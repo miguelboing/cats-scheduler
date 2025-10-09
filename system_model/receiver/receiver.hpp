71c58e36bebca6fe4e32d61759dc31712c5c6977
Miguel Boing <71512482+miguelboing@users.noreply.github.com>
2025-10-09 17:10:54 +0100

    system_model/receiver: Implementing output vector with received packets

--- system_model/receiver/receiver.hpp
+++ system_model/receiver/receiver.hpp
@@ -6,7 +6,7 @@
 class Receiver
 {
 public:
-    void recv_packets(system_model_t system_model, schedule_result_t schedule_result);
+    receiver_result_t recv_packets(system_model_t system_model, schedule_result_t schedule_result);
 private:
     std::default_random_engine generator;
 };
