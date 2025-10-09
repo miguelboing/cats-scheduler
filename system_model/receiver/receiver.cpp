58d2033b1cf03f8a18a8e842ce0397dfb2dde4a9
miguelboing <>
2025-10-09 17:10:54 +0100

    system_model/receiver: Implementing output vector with received packets

--- system_model/receiver/receiver.cpp
+++ system_model/receiver/receiver.cpp
@@ -5,21 +5,32 @@
 #include "system_model/system_model.hpp"
 #include "receiver.hpp"
 
-void Receiver::recv_packets(system_model_t system_model, schedule_result_t schedule_result)
+receiver_result_t Receiver::recv_packets(system_model_t system_model, schedule_result_t schedule_result)
 {
-    std::vector<bool> lost_frames;
     std::bernoulli_distribution distribution(0.0);
+    receiver_result_t receiver_result;
+    bool prob_result; /* The result of the bernoulli prob function */
 
     /*TODO: Change the probabilities based on the transmission power */
 
     /* Use the channel_condition to emulate packet loss */
-    for (const auto &channel_prob: *system_model.channel_condition)
+    for (unsigned int i=0; i< system_model.number_of_frames; ++i)
     {
-        distribution = std::bernoulli_distribution(channel_prob);
-        lost_frames.push_back(distribution(this->generator));
-        std::cout << channel_prob << ", " << lost_frames.back() << " |" ;
+        distribution = std::bernoulli_distribution((*system_model.channel_condition)[i]);
+        prob_result = (distribution(this->generator));
+
+        receiver_result.lost_frames.push_back(prob_result);
+
+        if (prob_result)
+        {
+            receiver_result.received_frames.push_back('0' + (char)(schedule_result.frame_allocation[i].task_id));
+        }
+        else
+        {
+            receiver_result.received_frames.push_back('X');
+        }
     }
 
-    std::cout << std::endl;
+    return receiver_result;
 }
 
