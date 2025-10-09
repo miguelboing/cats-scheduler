b69c50c6ef75c639f23d8a3f2c2d06c1d474d3b0
miguelboing <>
2025-10-09 15:48:44 +0100

    Support for normal values and template for prob distrib calculation

--- system_model/physical_channel/physical_channel.hpp
+++ system_model/physical_channel/physical_channel.hpp
@@ -7,6 +7,7 @@
 typedef enum /* This enum defines how the results will be generated */
 {
     BERNOULLI = 0,        /**< . */
+    NORMAL,
     NO_DISTR,
 } prob_distr_e;
 
@@ -21,6 +22,16 @@ public:
 
 private:
     std::default_random_engine generator;
-
+    template<typename Distribution>
+        void fill_channel_condition(Distribution& distribution)
+        {
+            for (unsigned int i = 0U; i < this->number_of_frames; ++i)
+            {
+                double value = (double) distribution(this->generator);
+                // Clamp value to [0, 1] for valid probability range
+                value = std::max(0.0, std::min(1.0, value));
+                (*this->channel_condition)[i] = value;
+            };
+        };
 };
 
