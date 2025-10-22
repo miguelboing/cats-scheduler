58ec886795820e54b6927e13f5421b182ed0fb39
miguelboing <miguelboing13@gmail.com>
2025-10-22 18:30:17 +0100

    system_model: Refactoring packet_id to id

--- system_model/system_model.hpp
+++ system_model/system_model.hpp
@@ -6,8 +6,7 @@
 
 typedef struct
 {
-//    unsigned int period;          /* T : Periodicy related with the task */
-    unsigned int packet_id;         /* ID: Tasks unique identifier */
+    unsigned int id;                /* ID: Tasks unique identifier */
     unsigned int deadline;          /* D : This is the relative deadline */
     unsigned int comp_cost;         /* C : A unit consumes one frame */
     unsigned int success_rate_req;  /* S : Success rate requirement for the packet */
