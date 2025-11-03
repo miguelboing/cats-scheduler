ffc94c65cec8784f6ed6671d38c346e5873fda44
miguelboing <miguelboing13@gmail.com>
2025-11-03 20:42:18 +0000

    schedulers/earliest_deadline_first: Updating edf to function with system_ticks #7

--- schedulers/earliest_deadline_first/edf_scheduler.cpp
+++ schedulers/earliest_deadline_first/edf_scheduler.cpp
@@ -4,112 +4,19 @@
 
 #include "edf_scheduler.hpp"
 
-EDF_scheduler::EDF_scheduler(unsigned int tx_power, unsigned int frequency): tx_power(tx_power), frequency(frequency) {};
+EDF_scheduler::EDF_scheduler(unsigned int tx_power, unsigned int frequency, std::shared_ptr<std::vector<packet_t>> buffer_packet): BaseScheduler(buffer_packet), tx_power(tx_power), frequency(frequency) {};
 
-schedule_result_t EDF_scheduler::schedule_packets(system_model_t system_model)
+packet_t& EDF_scheduler::schedule_packets(void)
 {
-   edf_packets.clear();
-    for (const auto& packet : *this->priv_queue_packet) {
-        edf_packets.emplace_back(packet);
-    }
-    schedule_result_t schedule_result;
-    schedule_result.is_feasible = true;
-
-    BaseScheduler::reset_scheduler();
-
-    while (this->time_frame_counter < system_model.number_of_frames)
-    {
-        /* Check if there is any missed deadline */
-        for (auto& packet : edf_packets)
-        {
-            /* Check if packet was scheduled */
-            if (packet.is_available)
-            {
-                /* Check if the packet missed its deadline */
-                if (((packet.deadline() + packet.comp_cost()) < this->time_frame_counter))
-                {
-                    std::cout << "Missed packet ID: " << packet.id() << std::endl;
-                    std::cout << "Missed packet deadline: " << packet.deadline() << std::endl;
-                    std::cout << "Current time_frame: " << this->time_frame_counter << std::endl;
-
-                    /* Updating deadline and recording packet*/
-                    schedule_result.missed_deadlines.push_back(packet.original_packet);  // Push original packet
-                    packet.deadline() += packet.period;
-
-                    schedule_result.is_feasible = false;
-                }
-            }
-            else /* Check if the unavailable (i.e. already scheduled) packet has arrived again */
-            {
-                if (packet.deadline() - packet.period <= this->time_frame_counter)
-                {
-                    packet.is_available = true;
-                }
-            }
-        }
-
-        edf_packet_t* lowest_packet = nullptr;
-
-        for (auto& packet : edf_packets) {
-            if (packet.is_available) {
-                if (lowest_packet == nullptr || packet.deadline() < lowest_packet->deadline()) {
-                    lowest_packet = &packet;
-                }
-            }
-        }
-
-        if (lowest_packet == nullptr) {
-            /* No packet is available, run idle */
-            this->time_frame_counter++;
-
-            schedule_result.frame_allocation.insert(schedule_result.frame_allocation.end(), 1, {0, 0, 0, 0, 0}); /* Schedule idle packet with comp_cost=1, id=0, ap and tx_power=0 */
-
-            continue; /* Go to iteration of the while */
-
-        }
-
-        /* Execute the packet transmission */
-        lowest_packet->is_available = false;
-        lowest_packet->packet_count++;
-        this->time_frame_counter += lowest_packet->comp_cost();
-
-        for (unsigned int frame_id = 1; frame_id <= lowest_packet->comp_cost(); ++frame_id)
-        {
-            schedule_result.frame_allocation.push_back({
-            lowest_packet->id(),             /* packet_id */
-            frame_id,                        /* packet_frame_id (1 to comp_cost) */
-            lowest_packet->packet_count,     /* packet_count */
-            this->tx_power,                  /* tx_power */
-            this->frequency                  /* frequency */
-            });
-        }
-
-        /* Update the deadline */
-        lowest_packet->deadline() += lowest_packet->period;
-    }
-
-    return schedule_result;
+   auto lowest_it = std::min_element(this->buffer_packet->begin(),
+                                     this->buffer_packet->end(),
+                                     [](const packet_t& a, const packet_t& b) {
+                                         return a.deadline < b.deadline;
+                                     });
+   return *lowest_it;
 }
 
-//const edf_packet_t EDF_scheduler::idle_task= edf_packet_t({0, UINT_MAX, 1, 100});
-//
 std::string EDF_scheduler::get_name() const {
     return "EDF";
 }
-
-//void EDF_scheduler::sort_packets_by_deadline()
-//{
-//    if (priv_queue_packet->empty()) {
-//        return {};
-//    }
-//
-//    /* Create a copy to avoid modifying original */
-//    //std::vector<packet_t> sorted_packets = *priv_queue_packet;
-//
-//    /* Sort by deadline (earliest first) */
-//    std::sort(priv_queue_packet.begin(), priv_queue_packet.end(),
-//        [](const packet_t& a, const packet_t& b) {
-//            return a.deadline < b.deadline;
-//        });
-//}
 
--- schedulers/earliest_deadline_first/edf_scheduler.hpp
+++ schedulers/earliest_deadline_first/edf_scheduler.hpp
@@ -4,44 +4,15 @@
 
 #include "schedulers/base_scheduler.hpp"
 
-struct edf_packet_t {
-    packet_t original_packet;  /* The original packet data */
-
-    /* EDF-specific internal parameters */
-    unsigned int original_deadline;    /* Store original deadline for reset */
-    unsigned int remaining_comp_cost;  /* Remaining computation time */
-    bool         is_available;         /* Whether packet is currently scheduled */
-    unsigned int last_execution_frame; /* Last frame when this packet was executed */
-    unsigned int period;               /* Period for periodic tasks */
-    unsigned int packet_count;         /* Number of times this packet has been transmited */
-
-    /* Constructor */
-    edf_packet_t(const packet_t& packet) : original_packet(packet) {
-        original_deadline = packet.deadline;
-        remaining_comp_cost = packet.comp_cost;
-        is_available = true;
-        period = packet.deadline; /* Assume deadline = period initially */
-        packet_count = 0;
-    }
-
-    /* Easy access to original fields */
-    unsigned int& id()               { return original_packet.id; }
-    unsigned int& deadline()         { return original_packet.deadline; }
-    unsigned int& comp_cost()        { return original_packet.comp_cost; }
-    unsigned int& success_rate_req() { return original_packet.success_rate_req; }
-};
-
-class EDF_scheduler : public BaseScheduler {
+class EDF_scheduler : public BaseScheduler
+{
 public:
-    EDF_scheduler(unsigned int tx_power, unsigned int frequency);
+    EDF_scheduler(unsigned int tx_power, unsigned int frequency, std::shared_ptr<std::vector<packet_t>> buffer_packet);
     unsigned int tx_power;
     unsigned int frequency;
-    schedule_result_t schedule_packets(system_model_t system_model) override;
+    packet_t& schedule_packets(void) override;
 
     std::string get_name() const override;
-private:
-      std::vector<edf_packet_t> edf_packets;
-//      static const edf_packet_t idle_task; /* This is run when no other task is available*/
 };
 
 
