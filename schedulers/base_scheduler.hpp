9753289a814f97b6346b5ab548475c6615e066df
miguelboing <miguelboing13@gmail.com>
2025-11-03 20:41:28 +0000

    schedulers: Update base_scheduler to point to buffer at start #7

--- schedulers/base_scheduler.hpp
+++ schedulers/base_scheduler.hpp
@@ -5,20 +5,23 @@
 
 #include "system_model/system_model.hpp"
 
-class BaseScheduler {
+class BaseScheduler
+{
 public:
-    virtual ~BaseScheduler() = default;
+    BaseScheduler(std::shared_ptr<std::vector<packet_t>> buffer);
 
-    void add_queue(std::shared_ptr<std::vector<packet_t>> queue_packet);
+    virtual ~BaseScheduler() = default;
 
     void reset_scheduler();
 
+    void change_buffer_packet(std::shared_ptr<std::vector<packet_t>> buffer);
+
     /* Pure virtual functions that all schedulers must implement */
-    virtual schedule_result_t schedule_packets(system_model_t system_model) = 0;
+    virtual packet_t& schedule_packets(void) = 0;
 
     virtual std::string get_name() const = 0;
 
-    std::shared_ptr<std::vector<packet_t>> queue_packet; /* Vector that points to the queue to be scheduled */
+    std::shared_ptr<std::vector<packet_t>> buffer_packet; /* Vector that points to the queue to be scheduled */
 
     schedule_result_t schedule_result;
 
@@ -28,12 +31,12 @@ protected:
     std::shared_ptr<std::vector<packet_t>> priv_queue_packet; /* Internal variable to handle iterations of queue_packet*/
 };
 
-inline void BaseScheduler::add_queue(std::shared_ptr<std::vector<packet_t>> queue_packet) {
-    this->queue_packet = queue_packet; /* Keep an untouched reference to the original queue */
-    this->priv_queue_packet = std::make_shared<std::vector<packet_t>>(*queue_packet); /* Make a copy of the queue to update deadlines */
+inline BaseScheduler::BaseScheduler(std::shared_ptr<std::vector<packet_t>> buffer)
+{
+    this->change_buffer_packet(buffer);
 }
 
-inline void BaseScheduler::reset_scheduler() {
-    this->priv_queue_packet = this->queue_packet;
+inline void BaseScheduler::change_buffer_packet(std::shared_ptr<std::vector<packet_t>> buffer) {
+    this->buffer_packet = buffer; /* Keep an untouched reference to the original queue */
 }
 
