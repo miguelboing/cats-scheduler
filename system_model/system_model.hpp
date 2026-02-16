0bb7754ce4c01c8b4d2d025b8a234a1877521cfd
miguelboing <miguelboing13@gmail.com>
2026-02-16 12:44:25 +0000

    system_model: Removing unused structs

--- system_model/system_model.hpp
+++ system_model/system_model.hpp
@@ -14,28 +14,6 @@ typedef struct
     double       success_rate_req;  /* S : Success rate requirement for the packet */
 } packet_t;
 
-struct channel_t
-{
-    unsigned int frequency;
-    std::vector<unsigned int> tx_power_levels;
-    std::shared_ptr<std::vector<std::vector<double>>> channel_condition; /* This is a 2D array frame x power */
-    size_t num_power_levels;
-
-    channel_t(unsigned int frequency, unsigned int num_frames, const std::vector<unsigned int>& tx_power_levels):
-        frequency(frequency), tx_power_levels(tx_power_levels),
-        num_power_levels(tx_power_levels.size())
-    {
-        channel_condition = std::make_shared<std::vector<std::vector<double>>>(
-        num_power_levels, std::vector<double>(num_frames));
-    }
-};
-
-typedef struct
-{
-    unsigned int number_of_frames;                     /* This is the total number of frames available to transmit */
-    std::vector<unsigned int> frequencies;             /* List of frequencies available for transmission */
-} system_model_t;
-
 typedef struct
 {
     packet_t* packet;
