9b188bc2ee2844528f92651cfa19395886df1e03
miguelboing <miguelboing13@gmail.com>
2026-02-20 16:05:24 +0000

    system_model/ml_predictor: Initial pseudo-ml_model #14

---
+++ system_model/ml_predictor/ml_predictor.cpp
@@ -0,0 +1,23 @@
+#include <random>
+#include <vector>
+#include <algorithm>
+
+#include "system_model/system_model.hpp"
+#include "ml_predictor.hpp"
+
+MLPredictor::MLPredictor(std::shared_ptr<unsigned int> sys_tick, std::shared_ptr<std::vector<SigmoidChannel>> sigmoid_channels): system_tick(sys_tick), channels(sigmoid_channels) {};
+
+double MLPredictor::predict_channel_conditions(unsigned int frequency, unsigned int transmission_power)
+{
+    auto it = std::find_if(channels.begin(), channels.end(),
+        [frequency](const SigmoidChannel& ch) {return ch.frequency == frequency;});
+
+    if (it != channels.end())
+    {
+        return it->gen_probability(transmission_power);
+    }
+
+    /* -1 represents that the MLPredictor didn't recognize the frequency */
+    return -1.0;
+}
+
---
+++ system_model/ml_predictor/ml_predictor.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <random>
+#include <vector>
+
+#include "system_model/system_model.hpp"
+
+class MLPredictor
+{
+public:
+    explicit MLPredictor(std::shared_ptr<unsigned int> sys_tick, std::shared_ptr<std::vector<SigmoidChannel>> sigmoid_channels);
+
+    double predict_channel_conditions(unsigned int frequency, unsigned int transmission_power);
+
+    std::shared_ptr<unsigned int> system_tick;
+
+    std::shared_ptr<std::vector<SigmoidChannel>> channels;
+};
+
