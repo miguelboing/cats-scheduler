ec37cb0c384cc627baa2f9c4dce672ac6bfe4bbc
miguelboing <miguelboing13@gmail.com>
2026-03-09 12:49:12 +0000

    schedulers/ Adding a virtual method to handle predictions #14

--- schedulers/base_scheduler.hpp
+++ schedulers/base_scheduler.hpp
@@ -21,6 +21,9 @@ public:
 
     virtual std::string get_name() const = 0;
 
+    /* Non Pure virtual function */
+    virtual void receive_prediction(double pred_dec_prob); /* This function handles the channel predictions */
+
     std::shared_ptr<std::vector<packet_t>> buffer_packet; /* Vector that points to the queue to be scheduled */
 };
 
@@ -29,7 +32,13 @@ inline BaseScheduler::BaseScheduler(std::shared_ptr<std::vector<packet_t>> buffe
     this->change_buffer_packet(buffer);
 }
 
-inline void BaseScheduler::change_buffer_packet(std::shared_ptr<std::vector<packet_t>> buffer) {
+inline void BaseScheduler::change_buffer_packet(std::shared_ptr<std::vector<packet_t>> buffer)
+{
     this->buffer_packet = buffer; /* Keep an untouched reference to the original queue */
+}
+
+inline void BaseScheduler::receive_prediction(double pred_dec_prob)
+{
+    return NULL;
 }
 
