7dfa5a65ce7ca9952febd39f3e4c625da875a78e
miguelboing <miguelboing13@gmail.com>
2026-03-13 16:17:23 +0000

    physical_channels/sigmoid_channel: Adding FSMC to model the channels #23

--- physical_channels/sigmoid_channel/sigmoid_channel.cpp
+++ physical_channels/sigmoid_channel/sigmoid_channel.cpp
@@ -1,23 +1,48 @@
 #include <cmath>
 #include <iostream>
+#include <fstream>
+#include <random>
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+#include <kovian/kovian.hpp>
+using namespace kovian::aliases;
 
 #include "sigmoid_channel.hpp"
 
-SigmoidChannel::SigmoidChannel(unsigned int frequency, double ref_power_w, double ref_psr, double pathloss, double noise, double s):
-    BasePhysicalChannel(frequency), slope(s), noise_floor_dbm(noise), pathloss_db(pathloss)
+SigmoidChannel::SigmoidChannel(unsigned int frequency, const std::string& channel_name):
+    BasePhysicalChannel(frequency), mc("physical_channels/" + channel_name + "/transition_matrix.kov")
 {
-    /* Calculate SNR for reference power */
-    double ref_tx_dbm = 10 * log10(ref_power_w * 1000);
-    double ref_rx_dbm = ref_tx_dbm - pathloss_db;
-    double ref_snr_db = ref_rx_dbm - noise_floor_dbm;
+    this->pathloss_db = 0;
 
-    /* Calculate SNR_50 based on reference PSR */
-    this->snr_50_db = ref_snr_db + (1.0/slope) * log((1.0 - ref_psr)/ref_psr);
+    /* Loading States */
+    std::ifstream f("physical_channels/" + channel_name + "/fsmc_states.json");
+
+    json data = json::parse(f);
+    for (const auto& s : data)
+    {
+        fsmc.push_back
+        ({
+            s["snr_50_db"],
+            s["slope"],
+            s["max_saturation"],
+            s["noise_floor_dbm"]
+        });
+    }
+
+    /* Defining initial state for fsmc */
+    std::uniform_int_distribution<int> distribution(0, 4);
+    this->mc.setState(distribution(generator));
 }
 
 double SigmoidChannel::gen_probability(unsigned int transmission_power)
 {
+    int fsmc_state;
     double tx_power_dbm, rx_power_dbm, snr_db;
+
+    /* Getting the current state */
+    fsmc_state = this->mc.current();
 
     /* Convert power W to dbmW */
     tx_power_dbm = 10 * log10(transmission_power * 1000);
@@ -26,10 +51,10 @@ double SigmoidChannel::gen_probability(unsigned int transmission_power)
     rx_power_dbm = tx_power_dbm - this->pathloss_db;
 
     /* SNR considering the noisefloor */
-    snr_db = rx_power_dbm - this->noise_floor_dbm;
+    snr_db = rx_power_dbm - this->fsmc[fsmc_state].noise_floor_dbm;
 
     /* Calculating the probability on the sigmoid slope */
-    return 1.0 / (1.0 + exp(-slope * (snr_db - snr_50_db)));
+    return 1.0 / (1.0 + exp(-(this->fsmc[fsmc_state].slope) * (snr_db - (this->fsmc[fsmc_state].snr_50_db))));
 }
 
 received_frame_t SigmoidChannel::gen_frame_with_probability(transmitted_frame_t transmitted_frame)
@@ -42,5 +67,10 @@ received_frame_t SigmoidChannel::gen_frame_with_probability(transmitted_frame_t 
     recv_frame.success_prob = this->gen_probability(recv_frame.transmission_power);
 
     return recv_frame;
+}
+
+void SigmoidChannel::advance_fsmc_state(void)
+{
+    (void)this->mc.advance();
 }
 
--- physical_channels/sigmoid_channel/sigmoid_channel.hpp
+++ physical_channels/sigmoid_channel/sigmoid_channel.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <kovian/kovian.hpp>
+
 #include "physical_channels/base_physical_channel.hpp"
 
 class MLPredictor;
@@ -9,16 +11,16 @@ class SigmoidChannel: public BasePhysicalChannel
 
 friend class MLPredictor;
 public:
-    explicit SigmoidChannel(unsigned int frequency, double ref_power_w=10, double ref_psr=0.8, double pathloss=100, double noise = -90.0, double s = 2.0);
+    explicit SigmoidChannel(unsigned int frequency, const std::string& channel_name);
 
     double gen_probability(unsigned int transmission_power) override;
     received_frame_t gen_frame_with_probability(transmitted_frame_t transmitted_frame) override;
+    void advance_fsmc_state(void);
 
+    std::vector<markov_state_t> fsmc;
+    kovian::MarkovChain<5> mc;
 private:
-    double snr_50_db;       /* beta: SNR for 50% success rate */
-    double slope;           /* alpha: Steepness of sigmoid curve */
-    double max_saturation;   /* gamma: The maximum possible transmission power when tx_power -> inf */
-
-    double noise_floor_dbm; /* Noise power in dBm */
+    std::default_random_engine generator;
     double pathloss_db;     /* Pathloss of the channel in dB*/
 };
+
