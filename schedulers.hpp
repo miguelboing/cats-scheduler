ad75b3a44264bd23b5e05d497aa567938e2bff5b
miguelboing <miguelboing13@gmail.com>
2026-03-23 15:04:48 +0000

    scheduler/channel_aware_spf: Adding initial channel aware sch arch #25

--- schedulers.hpp
+++ schedulers.hpp
@@ -2,4 +2,5 @@
 
 #include "schedulers/earliest_deadline_first/edf_scheduler.hpp"
 #include "schedulers/smaller_period_first/spf_scheduler.hpp"
+#include "schedulers/channel_aware_spf/channel_aware_spf_scheduler.hpp"
 
---
+++ schedulers/channel_aware_spf/channel_aware_spf_scheduler.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+#include "channel_aware_spf_scheduler.hpp"
+
+CHASPF_scheduler::CHASPF_scheduler(unsigned int tx_power, unsigned int frequency, std::shared_ptr<std::vector<packet_t>> buffer_packet, std::shared_ptr<unsigned int> sys_tick): BaseScheduler(buffer_packet, sys_tick), tx_power(tx_power), frequency(frequency) {};
+
+scheduled_frame_t CHASPF_scheduler::do_schedule_frame(void)
+{
+    scheduled_frame_t scheduled_frame;
+    scheduled_frame.transmission_power = this->tx_power;
+    scheduled_frame.frequency = this->frequency;
+
+    /* Find the packet with the smaller period */
+    auto lowest_it = std::min_element(this->buffer_packet->begin(),
+                                      this->buffer_packet->end(),
+                                      [](const packet_t& a, const packet_t& b) {
+                                          if (!a.is_periodic) return false;
+                                          if (!b.is_periodic) return true;
+                                          return a.period < b.period;
+                                      });
+
+    if (lowest_it != this->buffer_packet->end() && lowest_it->is_periodic)
+    {
+        scheduled_frame.packet = &(*lowest_it);
+        scheduled_frame.radio_mode = TX_MODE;
+    }
+    else
+    {
+        scheduled_frame.packet = nullptr; /* Means idle/no tranmission */
+        scheduled_frame.radio_mode = IDLE;
+    }
+
+    return scheduled_frame;
+}
+
+std::string CHASPF_scheduler::get_name() const {
+    return "Channel Aware SPF";
+}
+
