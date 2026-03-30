fa19af539af03470d89ee7a88305bf3e00c1ee64
miguelboing <miguelboing13@gmail.com>
2026-03-30 18:27:52 +0100

    system_model/ml_predictor: Changing prediction to accept multiple TX values #14

--- system_model/ml_predictor/ml_predictor.cpp
+++ system_model/ml_predictor/ml_predictor.cpp
@@ -7,17 +7,20 @@
 
 MLPredictor::MLPredictor(std::shared_ptr<unsigned int> sys_tick, std::shared_ptr<std::vector<SigmoidChannel>> sigmoid_channels): system_tick(sys_tick), channels(sigmoid_channels) {};
 
-double MLPredictor::predict_channel_conditions(unsigned int frequency, unsigned int transmission_power)
+std::vector<double> MLPredictor::predict_channel_conditions(unsigned int frequency, const std::vector<unsigned int>& powers)
 {
     auto it = std::find_if(channels->begin(), channels->end(),
         [frequency](const SigmoidChannel& ch) {return ch.frequency == frequency;});
 
     if (it != channels->end())
     {
-        return it->gen_probability(transmission_power);
+        std::vector<double> probs;
+        for (unsigned int power : powers)
+            probs.push_back(it->gen_probability(power));
+        return probs;
     }
 
     /* -1 represents that the MLPredictor didn't recognize the frequency */
-    return -1.0;
+    return std::vector<double>(powers.size(), -1.0);
 }
 
--- system_model/ml_predictor/ml_predictor.hpp
+++ system_model/ml_predictor/ml_predictor.hpp
@@ -12,7 +12,7 @@ class MLPredictor
 public:
     explicit MLPredictor(std::shared_ptr<unsigned int> sys_tick, std::shared_ptr<std::vector<SigmoidChannel>> sigmoid_channels);
 
-    double predict_channel_conditions(unsigned int frequency, unsigned int transmission_power);
+    std::vector<double> predict_channel_conditions(unsigned int frequency, const std::vector<unsigned int>& powers);
 
     std::shared_ptr<unsigned int> system_tick;
 
